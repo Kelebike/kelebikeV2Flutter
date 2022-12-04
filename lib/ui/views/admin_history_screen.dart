29bb76c16999a05c02be71b0050eec368fdad5f4
emirhansal <76394207+emirhansal@users.noreply.github.com>
2022-12-05 02:13:16 +0300

    deneme

--- lib/ui/views/admin_history_screen.dart
+++ lib/ui/views/admin_history_screen.dart
@@ -1,6 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+<<<<<<< HEAD
+=======
+import 'package:google_fonts/google_fonts.dart';
+
+>>>>>>> parent of e94be32 (ui change)
 import '../../core/services/history_service.dart';
 
 List<List<String>> itemList = [];
@@ -29,6 +34,7 @@ class _AdminHistoryScreenState extends State<AdminHistoryScreen> {
     var parts = s.split(',');
     parts[0] = parts[0]
         .toString()
+<<<<<<< HEAD
         .substring(7, parts[0].toString().length); //account 0
     parts[1] =
         parts[1].toString().substring(6, parts[1].toString().length); //code 1
@@ -36,6 +42,17 @@ class _AdminHistoryScreenState extends State<AdminHistoryScreen> {
         parts[3].toString().substring(8, parts[3].toString().length); //issued 3
     parts[5] =
         parts[5].toString().substring(8, parts[5].toString().length); //return 5
+=======
+        .substring(8, parts[0].toString().length); //owner 0
+    parts[1] =
+        parts[1].toString().substring(12, parts[1].toString().length); //issuedDate 1
+    parts[2] =
+        parts[2].toString().substring(7, parts[2].toString().length); //code
+    parts[3] =
+        parts[3].toString().substring(6, parts[3].toString().length); //issued 3
+    parts[5] =
+        parts[5].toString().substring(15, parts[5].toString().length); //return 5
+>>>>>>> parent of e94be32 (ui change)
 
     return parts;
   }
@@ -84,7 +101,11 @@ class _AdminHistoryScreenState extends State<AdminHistoryScreen> {
                       child: InkWell(
                         onTap: () {},
                         child: Container(
+<<<<<<< HEAD
                           height: size.height * .2,
+=======
+                          height: 130,
+>>>>>>> parent of e94be32 (ui change)
                           decoration: BoxDecoration(
                               color: Colors.white,
                               border: Border.all(
@@ -94,6 +115,7 @@ class _AdminHistoryScreenState extends State<AdminHistoryScreen> {
                           child: Padding(
                             padding: const EdgeInsets.all(1.0),
                             child: Column(
+<<<<<<< HEAD
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
                                 const Text("Bike: "),
@@ -106,6 +128,49 @@ class _AdminHistoryScreenState extends State<AdminHistoryScreen> {
                                 Text(infoBike[6]),
                                 const SizedBox(
                                   height: 10,
+=======
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: const [
+                                    Text("BİSİKLET",style: TextStyle(fontWeight: FontWeight.bold,color: Color(0xFF6CA8F1))),
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Text("Kod: ",style: TextStyle(fontWeight: FontWeight.bold)),
+                                    Text(infoBike[2]),
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Text("Kullanıcı: ",style: TextStyle(fontWeight: FontWeight.bold)),
+                                    Text(infoBike[0]),
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Text("Alım Tarihi: ",style: TextStyle(fontWeight: FontWeight.bold)),
+                                    Text(infoBike[1]),
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Text("Teslim Tarihi: ",style: TextStyle(fontWeight: FontWeight.bold)),
+                                    Text(infoBike[5]),
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Text("Kilit: ",style: TextStyle(fontWeight: FontWeight.bold)),
+                                    Text(infoBike[3]),
+                                  ],
+>>>>>>> parent of e94be32 (ui change)
                                 ),
                               ],
                             ),
--- lib/ui/views/admin_info_page.dart
+++ lib/ui/views/admin_info_page.dart
@@ -48,13 +48,21 @@ class _AdminInfoPageState extends State<AdminInfoPage> {
                           DocumentSnapshot mypost = snapshot.data!.docs[index];
 
                           return SizedBox(
+<<<<<<< HEAD
                             height: size.height * .55,
+=======
+                            height: size.height ,
+>>>>>>> parent of e94be32 (ui change)
                             child: ListView.builder(
                               scrollDirection: Axis.horizontal,
                               physics: const BouncingScrollPhysics(),
                               itemCount: 1,
                               itemBuilder: (context, index) {
+<<<<<<< HEAD
                                 return Row(
+=======
+                                return Column(
+>>>>>>> parent of e94be32 (ui change)
                                   children: [
                                     FutureBuilder<int>(
                                       future: _bikeService.totalBike(),
@@ -62,6 +70,7 @@ class _AdminInfoPageState extends State<AdminInfoPage> {
                                           AsyncSnapshot<int> snapshot) {
                                         if (snapshot.hasData) {
                                           return MyHorizontalList(
+<<<<<<< HEAD
                                             height: 349,
                                             width: 246,
                                             startColor: Colors.orange.shade100,
@@ -74,6 +83,19 @@ class _AdminInfoPageState extends State<AdminInfoPage> {
                                             courseImage:
                                                 'assets/logos/total.png',
                                             scale: 14,
+=======
+                                            height: 100,
+                                            width: size.width*0.9,
+                                            startColor: Colors.orange.shade100,
+                                            endColor: Color.fromARGB(255, 241, 113, 33),
+                                            courseHeadline: 'Toplam',
+                                            courseTitle:
+                                                'Toplam Bisiklet Sayısı : ' +
+                                                    '${snapshot.data}',
+                                            courseImage:
+                                                'assets/logos/total.png',
+                                            scale: 50,
+>>>>>>> parent of e94be32 (ui change)
                                           );
                                         } else {
                                           return const CircularProgressIndicator();
@@ -87,18 +109,32 @@ class _AdminInfoPageState extends State<AdminInfoPage> {
                                           AsyncSnapshot<int> snapshot) {
                                         if (snapshot.hasData) {
                                           return MyHorizontalList(
+<<<<<<< HEAD
                                             height: 349,
                                             width: 246,
                                             startColor: Colors.orange.shade100,
+=======
+                                            height: 100,
+                                            width: size.width*0.9,
+                                            startColor: Color.fromARGB(255, 205, 248, 241),
+>>>>>>> parent of e94be32 (ui change)
                                             endColor: const Color.fromARGB(
                                                 255, 54, 235, 244),
                                             courseHeadline: 'Uygun Bisiklet',
                                             courseTitle:
+<<<<<<< HEAD
                                                 'TOPLAM \nBISIKLET\nSAYISI : ' +
                                                     '${snapshot.data}',
                                             courseImage:
                                                 'assets/logos/available.png',
                                             scale: 1.8,
+=======
+                                                'Uygun Bisiklet Sayısı : ' +
+                                                    '${snapshot.data}',
+                                            courseImage:
+                                                'assets/logos/available.png',
+                                            scale: 4,
+>>>>>>> parent of e94be32 (ui change)
                                           );
                                         } else {
                                           return const CircularProgressIndicator();
@@ -112,19 +148,32 @@ class _AdminInfoPageState extends State<AdminInfoPage> {
                                           AsyncSnapshot<int> snapshot) {
                                         if (snapshot.hasData) {
                                           return MyHorizontalList(
+<<<<<<< HEAD
                                             height: 349,
                                             width: 246,
+=======
+                                            height: 100,
+                                            width: size.width*0.9,
+>>>>>>> parent of e94be32 (ui change)
                                             startColor: const Color.fromARGB(
                                                 255, 215, 212, 207),
                                             endColor:
                                                 const Color.fromARGB(255, 70, 81, 87),
                                             courseHeadline: 'Tamir',
                                             courseTitle:
+<<<<<<< HEAD
                                                 'TOPLAM TAMİRDEKİ \nBİSİKLET\nSAYISI : ' +
                                                     '${snapshot.data}',
                                             courseImage:
                                                 'assets/logos/repair.png',
                                             scale: 8,
+=======
+                                                'Tamirdeki Bisiklet Sayısı : ' +
+                                                    '${snapshot.data}',
+                                            courseImage:
+                                                'assets/logos/repair.png',
+                                            scale: 30,
+>>>>>>> parent of e94be32 (ui change)
                                           );
                                         } else {
                                           return const CircularProgressIndicator();
@@ -138,19 +187,32 @@ class _AdminInfoPageState extends State<AdminInfoPage> {
                                           AsyncSnapshot<int> snapshot) {
                                         if (snapshot.hasData) {
                                           return MyHorizontalList(
+<<<<<<< HEAD
                                             height: 349,
                                             width: 246,
+=======
+                                            height: 100,
+                                            width: size.width*0.9,
+>>>>>>> parent of e94be32 (ui change)
                                             startColor: const Color.fromARGB(
                                                 255, 225, 246, 133),
                                             endColor: const Color.fromARGB(
                                                 255, 0, 183, 110),
                                             courseHeadline: 'Dolaşım',
                                             courseTitle:
+<<<<<<< HEAD
                                                 'TOPLAM DOLAŞIMDAKİ \nBİSİKLET\nSAYISI : ' +
                                                     '${snapshot.data}',
                                             courseImage:
                                                 'assets/logos/circulation.png',
                                             scale: 16,
+=======
+                                                'Dolaşımdaki Bisiklet Sayısı : ' +
+                                                    '${snapshot.data}',
+                                            courseImage:
+                                                'assets/logos/circulation.png',
+                                            scale: 55,
+>>>>>>> parent of e94be32 (ui change)
                                           );
                                         } else {
                                           return const CircularProgressIndicator();
@@ -164,19 +226,32 @@ class _AdminInfoPageState extends State<AdminInfoPage> {
                                           AsyncSnapshot<int> snapshot) {
                                         if (snapshot.hasData) {
                                           return MyHorizontalList(
+<<<<<<< HEAD
                                             height: 349,
                                             width: 246,
+=======
+                                            height: 100,
+                                            width: size.width*0.9,
+>>>>>>> parent of e94be32 (ui change)
                                             startColor: const Color.fromARGB(
                                                 255, 191, 208, 172),
                                             endColor: const Color.fromARGB(
                                                 255, 125, 158, 214),
                                             courseHeadline: 'Alım İsteği',
                                             courseTitle:
+<<<<<<< HEAD
                                                 'TOPLAM \nALIM İSTEĞİ\nSAYISI : ' +
                                                     '${snapshot.data}',
                                             courseImage:
                                                 'assets/logos/take.png',
                                             scale: 9,
+=======
+                                                'Alım İsteği Sayısı : ' +
+                                                    '${snapshot.data}',
+                                            courseImage:
+                                                'assets/logos/take.png',
+                                            scale: 30,
+>>>>>>> parent of e94be32 (ui change)
                                           );
                                         } else {
                                           return const CircularProgressIndicator();
@@ -190,19 +265,32 @@ class _AdminInfoPageState extends State<AdminInfoPage> {
                                           AsyncSnapshot<int> snapshot) {
                                         if (snapshot.hasData) {
                                           return MyHorizontalList(
+<<<<<<< HEAD
                                             height: 349,
                                             width: 246,
+=======
+                                            height: 100,
+                                            width: size.width*0.9,
+>>>>>>> parent of e94be32 (ui change)
                                             startColor: const Color.fromARGB(
                                                 255, 240, 216, 232),
                                             endColor: const Color.fromARGB(
                                                 255, 196, 12, 147),
                                             courseHeadline: 'İade İsteği',
                                             courseTitle:
+<<<<<<< HEAD
                                                 'TOPLAM \nİADE İSTEĞİ\nSAYISI : ' +
                                                     '${snapshot.data}',
                                             courseImage:
                                                 'assets/logos/return.png',
                                             scale: 8,
+=======
+                                                'İade İsteği Sayısı: ' +
+                                                    '${snapshot.data}',
+                                            courseImage:
+                                                'assets/logos/return.png',
+                                            scale: 30,
+>>>>>>> parent of e94be32 (ui change)
                                           );
                                         } else {
                                           return const CircularProgressIndicator();
@@ -218,6 +306,7 @@ class _AdminInfoPageState extends State<AdminInfoPage> {
                   ),
                 ),
               ),
+<<<<<<< HEAD
               ClipRRect(
                 borderRadius: BorderRadius.circular(5.0),
                 child: Image.asset(
@@ -226,6 +315,8 @@ class _AdminInfoPageState extends State<AdminInfoPage> {
                   width: size.width,
                 ),
               )
+=======
+>>>>>>> parent of e94be32 (ui change)
             ],
           );
         });
